49a4afd12c403d1858206333555db41c0ee929b9
tsuahdolnik <tom@suhadolnik.com>
2024-06-03 20:33:36 -0400

    Committing from pycharm

--- test.py
+++ test.py
@@ -1,2 +1,3 @@
-if __name__ = "main":
-  print("This is the main file")
+if __name__ == "__main__":
+    print("This is the main file")
+    print("This is a change to see if I need to merge something up")
